59c489be4d3a2d95edfcbeb3c989796271b80a35
Alexei Goja <pliuwik@gmail.com>
2012-05-25 16:21:06 +0300

    Modified css file, added new templates and new image style

---
+++ templates/ding_nodelist.ding_news.text_after_image.tpl.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @file
+ * Ding event image and text template.
+ * Avaialable fields are:
+ * ding_content_tags
+ * field_address
+ * field_ding_body
+ * field_list_image
+ * field_main_image
+ * field_materials
+ * group_audience
+ */
+?>
+<div class="ding_news">
+  <h3><a href="<?php print url('node/' . $item->nid) ?>"><?php	print $item->title; ?></a></h3>
+  <div class="news_image">
+    <?php	print drupal_render(field_view_field('node', $item, 'field_list_image', 'teaser')); ?>
+  </div>
+  <div class="node">
+    <?php	print drupal_render(field_view_field('node', $item, 'field_ding_body', 'teaser')); ?>
+  </div>
+</div>
