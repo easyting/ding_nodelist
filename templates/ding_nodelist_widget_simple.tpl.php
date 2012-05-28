de307ea139b6c4f7e2c69e9d5f8adce93d13d601
Alex Oroshchuk <ao@inlead.dk>
2012-05-28 12:14:03 +0300

    case 462 - Fixed layout of different width columns.

--- templates/ding_nodelist_widget_simple.tpl.php
+++ templates/ding_nodelist_widget_simple.tpl.php
@@ -10,6 +10,7 @@
 ?>
 <?php if ($items): ?>
 <div class="<?php print $conf['classes']?>">
+  <h2 class="ding_nodelist-title"><?php print $conf['title']; ?></h2>
   <div class="ding_nodelist-items">
     <?php print $items; ?>
   </div>
