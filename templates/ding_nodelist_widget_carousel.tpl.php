0f8fe4dc00977ecb1b05a52a7296f7cfddbcfebf
Dragan <di@inlead.dk>
2012-05-31 13:39:40 +0300

    case 485 - Added carousel template.

---
+++ templates/ding_nodelist_widget_carousel.tpl.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @file
+ * Carousel list widget template.
+ * Variables are:
+ * $items - rendered items (HTML)
+ * $conf - list configuration with:
+ *  - classes - widget-specific CSS classes
+ */
+?>
+<?php if ($items): ?>
+<div class="<?php print $conf['classes']?>">
+  <h2 class="ding_nodelist-title"><?php print $conf['title']; ?></h2>
+  <div class="ding_nodelist-items">
+    <?php
+      foreach ($items as $node) {
+        print theme($template, array('item' => $node));
+      }
+    ?>
+  </div>
+  <div class="pagination"></div>
+</div>
+<?php endif; ?>
