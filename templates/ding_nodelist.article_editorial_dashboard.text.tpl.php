5f1401577ca4d06ea3ca168c56526af6872d369e
Dragan <di@inlead.dk>
2012-06-02 15:03:04 +0300

    case 483 - Added slider widget.

---
+++ templates/ding_nodelist.article_editorial_dashboard.text.tpl.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @file
+ * Ding event image and text template.
+ * Avaialable fields are:
+ * ding_content_tags
+ * field_address
+ * field_ding_body
+ * field_list_image
+ * field_main_image
+ * field_materials
+ * group_audience
+ */
+$edbase = taxonomy_term_load($item->field_eddb_article_base[LANGUAGE_NONE][0]['value']);
+?>
+<div class="item">
+  <span class="date-created">
+    <?php print date('d.m.Y', $item->created);?>
+  </span> - 
+  <span class="category">
+    <?php print l(t($edbase->name), 'taxonomy/term/' . $edbase->tid); ?>
+  </span>
+  <h3><a href="<?php print url('node/' . $item->nid) ?>"><?php	print $item->title; ?></a></h3>  
+  <div class="node">
+    <?php	print drupal_render(field_view_field('node', $item, 'body', 'teaser')); ?>
+  </div>
+  <div class="more"><?php print l(t('More'), 'node/' . $item->nid); ?></div>
+</div>
