a44c6ad0799c990fa96f7ab7ad53fb158d684b0a
Dragan <di@inlead.dk>
2012-06-02 18:01:24 +0300

    case 483 - Changes required for Ding CTs.

--- templates/ding_nodelist.ding_news.image_text.tpl.php
+++ templates/ding_nodelist.ding_news.image_text.tpl.php
@@ -11,16 +11,17 @@
  * field_materials
  * group_audience
  */
-$edbase = taxonomy_term_load($item->field_eddb_article_base[LANGUAGE_NONE][0]['value']);
+
+$edbase = taxonomy_term_load($item->field_editorial_base_n[LANGUAGE_NONE][0]['value']);
 ?>
 <div class="item">
-  <span class="date-created">
-    <?php print date('d.m.Y', $item->created);?>
-  </span> - 
-  <span class="category">
-    <?php print l(t($edbase->name), 'taxonomy/term/' . $edbase->tid); ?>
-  </span>
-  <h3><a href="<?php print url('node/' . $item->nid) ?>"><?php	print $item->title; ?></a></h3>  
+  <div class="category"><?php print l(t($edbase->name), 'taxonomy/term/' . $edbase->tid); ?></div>  
+  <h3><a href="<?php print url('node/' . $item->nid) ?>"><?php	print $item->title; ?></a></h3>
+  
+  <div class="article_image">
+    <?php	print drupal_render(field_view_field('node', $item, 'field_eddb_article_limage', 'teaser')); ?>
+  </div>
+  
   <div class="node">
     <?php	print drupal_render(field_view_field('node', $item, 'body', 'teaser')); ?>
   </div>
