9117a955ae7ad228829fecd516c424b0934dd3ed
Alex Oroshchuk <ao@inleadmedia.com>
2012-06-02 18:19:43 -0400

    Commiting AC changes.

--- templates/ding_nodelist.ding_event.image_text.tpl.php
+++ templates/ding_nodelist.ding_event.image_text.tpl.php
@@ -19,10 +19,12 @@ $library = field_get_items('node', $item, 'field_eddb_event_library');
 $library = node_load($library[0]['value']);
 ?>
 <li class="event">
-  <div class="event_date">
-    <?php echo date('d M', $event_date);?>
+  <div class="expand">Expand</div>
+  <div class="event-date">
+    <div class="event-day"><?php echo date('d', $event_date);?></div>
+    <div class="event-month"><?php echo date('M', $event_date);?></div>
   </div>
-  <?php print theme('image_style', array('style_name' => 'node_list_accordeon', 'path' => $image[0]['#item']['uri']));?>
+  <div class="event-image"><?php print theme('image_style', array('style_name' => 'node_list_accordeon', 'path' => $image[0]['#item']['uri']));?></div>
   <div class="data">
     <div class="event_time">
       Kl. <?php echo date('H:i', $event_date);?>
