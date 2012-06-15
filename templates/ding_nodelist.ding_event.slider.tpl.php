9ec753cd31d24bc24299bc29a4505ecf54828784
Dragan <di@inlead.dk>
2012-06-15 16:31:26 +0300

    case 578 - Added slider template for event.

---
+++ templates/ding_nodelist.ding_event.slider.tpl.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @file
+ * Ding event image and text template.
+ * Avaialable fields are:
+ * ding_content_tags
+ * field_address
+ * field_ding_body
+ * field_list_image
+ * field_main_image
+ * field_materials
+ * group_audience
+ */
+$edbase = field_view_field('node', $item, 'field_editorial_base_e', 'teaser');
+$event_date = field_get_items('node', $item, 'field_event_date');
+?>
+<div class="item">
+  <span class="date-created">
+    <?php print date('d.m.Y', strtotime($event_date[0]['value']));?>
+  </span> - 
+  <span class="category">
+    <?php print drupal_render($edbase); ?>
+  </span>
+  <h3><a href="<?php print url('node/' . $item->nid) ?>"><?php	print $item->title; ?></a></h3>  
+  <div class="node">
+    <?php	print drupal_render(field_view_field('node', $item, 'field_ding_body', 'teaser')); ?>
+  </div>
+  <div class="more"><?php print l(t('More'), 'node/' . $item->nid); ?></div>
+</div>
