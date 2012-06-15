56b19ad26c7bb13a1fbf5f8bffd846487c141556
Dragan <di@inlead.dk>
2012-06-15 14:37:10 +0300

    case 578 - Removed hardcode from vertical accordion init file.

--- js/vaccordion.js
+++ js/vaccordion.js
@@ -2,10 +2,15 @@
   $(document).ready(function(){
     $('.ding_nodelist-vertical_accordion').each(function(){
       var c_width = $(this).width();
-      console.log($(this).find('.va-slice:first').height());
+      var sum_height = 0;
+      $(this).find('.va-slice').each(function(){
+        sum_height += $(this).height();
+      });
+      sum_height /= 1.8;
+      console.log(Math.round(sum_height));
       $(this).vaccordion({
-        accordionH: 450,
-        expandedHeight: 350,
+        accordionH: Math.round(sum_height),
+        expandedHeight: $(this).find('.va-slice:first').height(),
         accordionW: c_width,
         animOpacity: 0.2
       });
