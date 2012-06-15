6123eef31fb880369cda35ed4ad1ab03c4c329e7
Dragan <di@inlead.dk>
2012-06-15 15:10:23 +0300

    case 578 - Added template for event single view.

---
+++ templates/ding_nodelist.ding_event.single.tpl.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @file
+ * Ding event image and text template.
+ * Avaialable fields are:
+ * ding_content_tags
+ * field_address
+ * field_ding_body
+ * field_list_image
+ * field_main_image
+ * field_materials
+ * group_audience
+ */
+
+$image = field_view_field('node', $item, 'field_list_image', 'teaser');
+$edbase = field_view_field('node', $item, 'field_editorial_base_e', 'teaser');
+$event_date = field_get_items('node', $item, 'field_event_date');
+$event_date = strtotime($event_date[0]['value']);
+?>
+<div class="item">
+	<div class="event-image">
+    <?php 
+    if ($image) {
+      print theme('image_style', array('style_name' => $conf['image_style'], 'path' => $image[0]['#item']['uri']));
+    }
+    ?>
+  </div>
+  <div class="event-date">
+    <div class="event-day"><?php echo date('d', $event_date);?></div>
+    <div class="event-month"><?php echo date('M', $event_date);?></div>
+  </div>
+  <div class="article-info">
+    <div class="category"><?php print drupal_render($edbase); ?></div>
+    <h3><a href="<?php print url('node/' . $item->nid) ?>"><?php	print $item->title; ?></a></h3>
+    <div class="node">
+      <?php	print drupal_render(field_view_field('node', $item, 'field_ding_body', 'teaser')); ?>
+    </div>
+    <div class="more"><?php print l(t('More'), 'node/' . $item->nid); ?></div>
+  </div>
+</div>
