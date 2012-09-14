e9931bff42bf17f18aebb01931ff10bb1d36119f
Anna Caraman <ac@inleadmedia.com>
2012-09-14 11:31:12 +0300

    case 1070 - Taxonomy like widget templates are changed.

--- templates/ding_nodelist.ding_event.taxonomy.tpl.php
+++ templates/ding_nodelist.ding_event.taxonomy.tpl.php
@@ -7,6 +7,7 @@
 
 $title = $item->title;
 $body = drupal_render(field_view_field('node', $item, 'field_ding_body', 'teaser'));
+$category = drupal_render(field_view_field('node', $item, 'group_audience', 'teaser'));
 $image = field_view_field('node', $item, 'field_list_image', 'teaser');
 $image = theme('image_style', array('style_name' => $conf['image_style'], 'path' => $image[0]['#item']['uri']));
 $date = field_get_items('node', $item, 'field_event_date');
@@ -14,7 +15,7 @@ $date = strtotime($date[0]['value']);
 if (!empty($item->publish_on)) {
   $date = $item->publish_on;
 }
-$date = date('D, d/m/Y - H:i', $date);
+$date = date('d.m.Y - H:i', $date);
 $author = $item->name;
 
 /**
@@ -32,11 +33,12 @@ $author = $item->name;
  *   Node author name.
  */
 ?>
-<div class="item">
+<div class="item event-item">
   <div class="item-list-image"><?php print $image ?></div>
   <div class="item-details">
+    <div class="item-category"><?php print $category ?></div>
+	<span class="item-date"><?php print $date ?></span>
     <h2 class="item-title"><?php print l($title, 'node/' . $item->nid); ?></h2>
-    <span class="item-date"><?php print $date ?></span><span class="item-author"><?php print $author ?></span>
     <div class="item-body"><?php print $body ?></div>
   </div>
 </div>
--- templates/ding_nodelist.ding_page.taxonomy.tpl.php
+++ templates/ding_nodelist.ding_page.taxonomy.tpl.php
@@ -6,13 +6,14 @@
  */
 $title = $item->title;
 $body = drupal_render(field_view_field('node', $item, 'field_ding_body', 'teaser'));
+$category = drupal_render(field_view_field('node', $item, 'field_category', 'teaser'));
 $image = field_view_field('node', $item, 'field_list_image', 'teaser');
 $image = theme('image_style', array('style_name' => $conf['image_style'], 'path' => $image[0]['#item']['uri']));
 $date = $item->created;
 if (!empty($item->publish_on)) {
   $date = $item->publish_on;
 }
-$date = date('D, d/m/Y - H:i', $date);
+$date = date('d.m.Y - H:i', $date);
 $author = $item->name;
 /**
  * Available variables:
@@ -29,11 +30,12 @@ $author = $item->name;
  *   Node author name.
  */
 ?>
-<div class="item">
+<div class="item page-item">
   <div class="item-list-image"><?php print $image ?></div>
   <div class="item-details">
+    <div class="item-category"><?php print $category ?></div>
+	<span class="item-date"><?php print $date ?></span>
     <h2 class="item-title"><?php print l($title, 'node/' . $item->nid); ?></h2>
-    <span class="item-date"><?php print $date ?></span><span class="item-author"><?php print $author ?></span>
     <div class="item-body"><?php print $body ?></div>
   </div>
 </div>
