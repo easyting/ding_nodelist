cf484aec074f3d131f7d23fc95886a3c44e11240
AnatolyMuntean <am@inlead.dk>
2012-10-18 12:10:05 +0300

    case 1351 - Template logic for two items, context output for plugin.

--- templates/ding_nodelist_widget_two_col.tpl.php
+++ templates/ding_nodelist_widget_two_col.tpl.php
@@ -14,13 +14,21 @@
 <div class="<?php print $conf['classes']?>">
   <h2 class="ding_nodelist-title"><?php print $conf['title']; ?></h2>
   <div class="ding_nodelist-items">
+    <div class="row">
     <?php
       // @todo
       // Add logic for splitting every two items into separate rows.
+      $i = 0;
       foreach ($items as $node) {
+        if ($i % 2 == 0 && $i) {
+          echo '</div>';
+          echo '<div class="row">';
+        }
         print theme($template, array('item' => $node));
+        $i++;
       }
     ?>
+    </div>
   </div>
 </div>
 <?php endif; ?>
