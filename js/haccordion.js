4395764905fd4d2b1ae01a13ac9cd44eb2222c42
lexor <ao@inlead.dk>
2012-10-22 11:15:17 +0300

    case 1242 - Speed check for horizontal accordion.

--- js/haccordion.js
+++ js/haccordion.js
@@ -3,6 +3,9 @@
     $('.ding_nodelist-horizontal_accordion').each(function(){
       var c_width = $(this).width();
       var classes = $(this).attr('class').split(' ');
+      // Slide change speed must not be greater than the pause
+      // and has a default value of 1200.
+      var anim_speed = 1200;
       delay = 0;
       // Find pane's ID to get its delay settings.
       $(classes).each(function(i, item){
@@ -10,8 +13,13 @@
           delay = parseInt(Drupal.settings.ding_nodelist[item]);
         }
       })
+      // Ensure the speed at least equals the timeout.
+      if (anim_speed > delay) {
+        anim_speed = delay;
+      }
       $(this).find('.ding_nodelist-items').zAccordion({
         timeout: delay,
+        speed: anim_speed,
         width: c_width,
         height: 350,
         slideClass: 'slide'
