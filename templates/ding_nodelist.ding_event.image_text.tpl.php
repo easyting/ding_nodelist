73ddc447d7937210a9e9a76591eed0fc6666de20
Alex Semeniuc <as@inlead.dk>
2013-03-14 15:08:27 +0200

    case 2334 - Added stripping all tags from node body.
    Also fixed some strict warnings.

--- templates/ding_nodelist.ding_event.image_text.tpl.php
+++ templates/ding_nodelist.ding_event.image_text.tpl.php
@@ -35,7 +35,10 @@ $event_date = _ding_nodelist_get_event_date($item);
         <span><?php print t('Time');?></span>
         <span><?php print format_date($event_date, 'custom', 'H:i');?></span>
       </div>
-      <?php print drupal_render(field_view_field('node', $item, 'group_audience', 'teaser'));?>
+      <?php
+        $audience = field_view_field('node', $item, 'group_audience', 'teaser');
+        print drupal_render($audience);
+      ?>
       <div class="event-fee">
       <?php
         $fee = current(field_get_items('node', $item, 'field_event_fee'));
