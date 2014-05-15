09605c743cab08cfc87ae0006f9cfa6c799b4a2c
Andrei Bunulu <bunulu@gmail.com>
2014-05-15 07:23:05 +0000

    case-4172 - Template changes

--- templates/ding_nodelist.ding_news.carousel.tpl.php
+++ templates/ding_nodelist.ding_news.carousel.tpl.php
@@ -20,7 +20,7 @@ $edbase = field_view_field('node', $item, 'field_editorial_base_n', 'teaser');
     <a href="<?php print url('node/' . $item->nid);?>"><?php print $image ? theme('image_style', array_merge($image, array('style_name' => $conf['image_style']))) : ''; ?></a>
   </div>
   <div class="article-info">
-  <div class="category"><?php print drupal_render($edbase);?></div>
+    <div class="category"><?php print drupal_render($edbase);?></div>
     <h3><a href="<?php print url('node/' . $item->nid);?>"><?php print $item->title;?></a></h3>
     <div class="node">
       <p>
